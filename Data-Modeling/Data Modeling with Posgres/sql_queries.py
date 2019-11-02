629e4e00cfb36c43fe65b929615fa1854b7eed65
Alen Mrsic <Alen.Mrsic@infobip.com>
2019-11-02 13:07:36 +0100

    first attempt of sql_queries

--- Data-Modeling/Data Modeling with Posgres/sql_queries.py
+++ Data-Modeling/Data Modeling with Posgres/sql_queries.py
@@ -11,53 +11,148 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays (
         songplay_id INTEGER ,
-        start_time TIMESTAMP NOT NULL,
+        start_time TIMESTAMP NOT NULL REFERENCES time(start_time), ,
         user_id INTEGER NOT NULL REFERENCES users (user_id),
-        level VARCHAR,
-        song_id VARCHAR REFERENCES songs (song_id),
-        artist_id VARCHAR REFERENCES artists (artist_id),
+        level TEXT,
+        song_id TEXT REFERENCES songs (song_id),
+        artist_id TEXT REFERENCES artists (artist_id),
         session_id INTEGER NOT NULL,
-        location VARCHAR,
-        user_agent VARCHAR
+        location TEXT,
+        user_agent TEXT
     )
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        gender CHAR(1),
+        level TEXT NOT NULL
+    )
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id TEXT PRIMARY KEY,
+        title TEXT NOT NULL,
+        artist_id TEXT NOT NULL REFERENCES artists (artist_id),
+        year INTEGER NOT NULL,
+        duration FLOAT NOT NULL
+    )
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id TEXT PRIMARY KEY,
+        name TEXT NOT NULL,
+        location TEXT,
+        latitude FLOAT,
+        longitude FLOAT
+    )
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time (
+        start_time TIMESTAMP NOT NULL PRIMARY KEY,
+        hour NUMERIC NOT NULL,
+        day NUMERIC NOT NULL,
+        week NUMERIC NOT NULL,
+        month NUMERIC NOT NULL,
+        year NUMERIC NOT NULL,
+        weekday NUMERIC NOT NULL
+    )
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays (
+        songplay_id,
+        start_time,
+        user_id,
+        level,
+        song_id,
+        artist_id,
+        session_id,
+        location,
+        user_agent
+    )
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
+
+    INSERT INTO users (
+        user_id,
+        first_name,
+        last_name,
+        gender,
+        level
+    )
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id)
+    DO NOTHING
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs (
+        song_id,
+        title,
+        artist_id,
+        year,
+        duration
+    )
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id)
+    DO NOTHING
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists (
+        artist_id,
+        name,
+        location,
+        latitude,
+        longitude
+    )
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id)
+    DO NOTHING
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO time (
+        start_time,
+        hour,
+        day,
+        week,
+        month,
+        year,
+        weekday
+    )
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time)
+    DO NOTHING
 """)
 
 # FIND SONGS
 
 song_select = ("""
+    SELECT
+        s.song_id AS song_id,
+        s.artist_id AS artist_id
+    FROM
+        songs s
+        JOIN artists a ON (s.artist_id = a.artist_id)
+    WHERE
+        s.title = %s AND
+        a.name = %s AND
+        s.duration = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [time_table_create, user_table_create, artist_table_create, song_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
